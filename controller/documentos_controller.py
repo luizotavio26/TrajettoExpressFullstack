bf09119ec033601907277c3fe5dd11750e7905cc
JoaoPedro-SA <antunesjoaopedro3@gmail.com>
2025-11-06 20:50:55 -0300

    ajuste da emissao de documento de carga

--- controller/documentos_controller.py
+++ controller/documentos_controller.py
@@ -1,7 +1,4 @@
 from flask import Blueprint, request, jsonify
-from model.documentos.tests.test import download_pdf
-from model.documentos.tests.test2 import *
-from model.documentos.tests.test3 import *
 from model.documentos.documentos_motorista import relatorioDeTodosMotoristas
 from model.documentos.documentos_veiculos import relatorioDeTodosVeiculos
 from model.documentos.documentos_cargas import relatorioDeTodasCargas
@@ -15,16 +12,6 @@ documentos = Blueprint('documentos', __name__)
 def test():
     return jsonify({"message":"Documentos Blueprint funcionando!"}), 200
 
-@documentos.route("/test/<int:id>", methods=['GET'])
-def get_documento(id):
-    if id == 1: 
-        return download_pdf()
-    elif id == 2:
-        return gerar_relatorio()
-    elif id == 3:
-        return gerar_faturamento()
-    else:
-        return jsonify({"message": f"Documentos Blueprint funcionando! ID recebido: {id}"}), 200
 
 @documentos.route("/relatorio/motoristas", methods=['GET'])
 def relatorio_motoristas():
